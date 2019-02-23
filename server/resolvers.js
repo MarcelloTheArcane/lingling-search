f7bba6f5cfd5af3347d0a61f2891e84280e78c8c
glenndevenish <max@gourmade.com>
2019-02-23 10:54:36 +0000

    Fix: add return

--- server/resolvers.js
+++ server/resolvers.js
@@ -3,7 +3,7 @@ const musicList = require('./db/music.js')
 const resolvers = {
   Query: {
     songsFromNotes: (parent, args, context, info) => {
-      musicList
+      return musicList
         .filter(song => {
           const SongNoteNames = song.notes.map(note => note.name.slice(0, 1)).join(' ')
           return SongNoteNames.includes(args.notes) // 'A B C'.includes('B C')?

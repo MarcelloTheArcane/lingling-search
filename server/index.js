949c61895663524b1e58b0d496f748b94a2c942e
glenndevenish <max@gourmade.com>
2019-02-23 10:44:54 +0000

    Proof of concept

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const { ApolloServer, gql } = require('apollo-server')
+const resolvers = require('./resolvers.js')
+
+const typeDefs = gql`
+type Query {
+  songsFromNotes (notes: [String!]!): [song]!
+  songsFromName (name: String!): [song]!
+}
+
+type song {
+  name: String
+}
+`
+
+const server = new ApolloServer({ typeDefs, resolvers })
+
+server.listen().then(({ url }) => {
+  console.log('Server ready at', url)
+})
---
+++ server/resolvers.js
@@ -0,0 +1,21 @@
+const musicList = require('./db/music.js')
+
+const resolvers = {
+  Query: {
+    songsFromNotes: (parent, args, context, info) => {
+      musicList
+        .filter(song => {
+          const SongNoteNames = song.notes.map(note => note.name.slice(0, 1)).join(' ')
+          return SongNoteNames.includes(args.notes.join(' ')) // 'A B C'.includes('B C')?
+        })
+        .map(song => song.header)
+    },
+    songsFromName: (parent, args, context, info) => {
+      return musicList
+        .filter(song => song.header.name === args.name)
+        .map(song => song.header)
+    },
+  }
+}
+
+module.exports = resolvers
